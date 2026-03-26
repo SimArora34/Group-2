4ef91f868421e9db84b8befda25b0b4edc908ab4
thiegorechi <rechithiego@gmail.com>
2026-03-25 21:14:06 -0600

    Register all new wallet screens in layout

--- ContriibiaApp/app/(wallet)/_layout.tsx
+++ ContriibiaApp/app/(wallet)/_layout.tsx
@@ -16,6 +16,16 @@ export default function WalletLayout() {
       <Stack.Screen name="add-money" options={{ title: 'Add money' }} />
       <Stack.Screen name="withdraw-money" options={{ title: 'Withdraw Money' }} />
       <Stack.Screen name="confirmation" options={{ title: 'Confirmation', headerLeft: () => null }} />
+      <Stack.Screen name="add-new-card" options={{ title: 'Add new card' }} />
+      <Stack.Screen name="setup-auto-load" options={{ title: 'Setup Auto Load' }} />
+      <Stack.Screen name="send-money" options={{ title: 'Send Money' }} />
+      <Stack.Screen name="confirm-send" options={{ title: 'Confirm Transaction' }} />
+      <Stack.Screen name="transaction-history" options={{ title: 'Transaction History' }} />
+      <Stack.Screen name="generate-invoice" options={{ title: 'Invoice Generator' }} />
+      <Stack.Screen name="invoice-send-user" options={{ title: 'Invoice Generator' }} />
+      <Stack.Screen name="biometrics" options={{ title: 'App Wallet' }} />
+      <Stack.Screen name="more" options={{ title: 'More' }} />
+      <Stack.Screen name="qr-code" options={{ title: 'App Wallet' }} />
     </Stack>
   );
 }
